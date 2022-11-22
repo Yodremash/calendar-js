886cdfd8ad5afdacb35644062e88e0797362f697
Alexey <bumbumbu2@gmail.com>
2022-11-22 16:20:43 +0200

    init files

--- scripts/common/createNumbersArray.js
+++ scripts/common/createNumbersArray.js
@@ -1,7 +1,7 @@
 const createNumbersArray = (from, to) => {
   // ф-ция должна генерировать массив чисел от from до to
   const newArray = [];
-  for (let i = from; i <= to; i++) {
+  for (let i = from; i <= to; i += 1) {
     console.log(i);
     newArray.push(i);
   }
